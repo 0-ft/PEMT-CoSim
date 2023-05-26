b7ec9654db50b40493d49b159d994fd3bf1a6a46
0-ft <0ft@pm.me>
2023-05-26 18:03:16 +0100

    remove object json stuff

--- demo-PET/generate_case.py
+++ demo-PET/generate_case.py
@@ -7,11 +7,11 @@ modified by Yuanliang Li
 
 """
 
-import os
 import numpy as np
-from glmhelper import GLM_HELPER
+
 import my_tesp_support_api.api as tesp
 from fed_weather.TMY3toCSV import weathercsv
+from glmhelper import GLM_HELPER
 
 """0. generate a glm file (TE_Challenge.glm) according to user's preference"""
 
