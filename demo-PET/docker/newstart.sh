7307a6fdbcfac27f6f3b02b27baa05bd7900ab56
0-ft <0ft@pm.me>
2023-06-07 17:28:22 +0100

    fix dockerise

--- demo-PET/docker/newstart.sh
+++ demo-PET/docker/newstart.sh
@@ -4,4 +4,4 @@ docker run -it --privileged -p 5953:5901 \
     --env="QT_X11_NO_MITSHM=1" \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
     --volume="$HOME/.Xauthority:/root/.Xauthority:rw" \
-    --mount type=bind,source=/home/atlas/sync/uni/year3/toshiba/PEMT-CoSim,destination=/PEMT-CoSim --name helics4 new-helics
+    --mount type=bind,source=/home/atlas/sync/uni/year3/toshiba/PEMT-CoSim,destination=/PEMT-CoSim --name evst1 evst
--- demo-PET/fed_gridlabd/checkpubs.py
+++
@@ -1,8 +0,0 @@
-import collections
-import json
-data = json.load(open("TE_Challenge_HELICS_gld_msg.json", "r"))
-
-keys = [x["key"] for x in data["publications"]]
-print(keys)
-print(len(keys), len(set(keys)))
-print([item for item, count in collections.Counter(keys).items() if count > 1])
