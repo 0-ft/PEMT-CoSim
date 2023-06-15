083ecec86606d32033865e8c020ba498fe049d78
0-ft <0ft@pm.me>
2023-06-15 15:42:45 +0100

    scenario names

--- batch_pem2.sh
+++ batch_pem2.sh
@@ -1,8 +1,8 @@
-python3 generate_case.py -n 30 -e 0 -p 0 -g 99999999 -w 0 -b 0.0 -s 0.1
+python3 generate_case.py -n 30 -e 0 -p 0 -g 99999999 -w 0 -b 0.0 -s 0.1 --name uncapped_0ev_0pv
 pkill helics_broker & helics run --path=runner.json
 
-python3 generate_case.py -n 30 -e 30 -p 30 -g 100000 -w 0 -b 0.0 -s 0.1
+python3 generate_case.py -n 30 -e 30 -p 30 -g 100000 -w 0 -b 0.0 -s 0.1 --name capped_30ev_30pv
 pkill helics_broker & helics run --path=runner.json
 
-python3 generate_case.py -n 30 -e 30 -p 30 -g 70000 -w 0 -b 0.0 -s 0.1
+python3 generate_case.py -n 30 -e 30 -p 30 -g 70000 -w 0 -b 0.0 -s 0.1 --name lowcap_30ev_30pv
 pkill helics_broker & helics run --path=runner.json
