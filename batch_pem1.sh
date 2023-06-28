e0267d5e9623d84af1486bc99b3977f70f1556c2
0-ft <0ft@pm.me>
2023-06-28 10:00:58 +0100

    remove profiles

--- batch_pem1.sh
+++
@@ -1,8 +0,0 @@
-python3 generate_case.py -n 30 -e 0 -p 0 -g 100000 -w 0 -b 0.0 -s 0.1 --name capped_0pv_0ev
-pkill helics_broker & helics run --path=runner.json
-
-python3 generate_case.py -n 30 -e 30 -p 0 -g 100000 -w 0 -b 0.0 -s 0.1 --name capped_0pv_30ev
-pkill helics_broker & helics run --path=runner.json
-
-python3 generate_case.py -n 30 -e 0 -p 30 -g 100000 -w 0 -b 0.0 -s 0.1 --name capped_30pv_0ev
-pkill helics_broker & helics run --path=runner.json
