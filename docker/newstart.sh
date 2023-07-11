a2c2b04d499eeadda74cf5bc56a0d8c4ca64d55c
0-ft <0ft@pm.me>
2023-07-11 11:11:06 +0100

    before removal of unused files

--- docker/newstart.sh
+++
@@ -1,7 +0,0 @@
-#docker rm helics2
-docker run -it --privileged -p 5953:5901 \
-    --env="DISPLAY" \
-    --env="QT_X11_NO_MITSHM=1" \
-    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-    --volume="$HOME/.Xauthority:/root/.Xauthority:rw" \
-    --mount type=bind,source=/home/atlas/sync/uni/year3/toshiba/PEMT-CoSim,destination=/PEMT-CoSim --name evst1 evst
